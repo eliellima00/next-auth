fc6bb8bc9eedea5604d55abab54d6b890b58beb3
eliellima00 <eliellimamatos@gmail.com>
2024-02-27 21:16:33 -0400

    feat: extending session and jwt callbacks

--- auth.ts
+++ auth.ts
@@ -1,9 +1,38 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
+import { getUserById } from "./data/user"
 import { db } from "./lib/db"
 
+type Role = "ADMIN" | "USER"
+
+declare module "next-auth" {
+  interface User {
+    role: Role
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.role = token.role as Role
+      }
+      return session
+    },
+    async jwt({
+      token
+    }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
